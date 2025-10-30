87de099f28f3069fa64b47c7ec15e8324bfc00f1
Olubukola Abraham <graceolubukola1105@gmail.com>
2025-10-30 09:28:46 +0100

    guess_number_graceroyale.md

---
+++ guess-the-number.py
@@ -0,0 +1,14 @@
+import random
+guess = None
+number = random.randint(1, 10)
+while guess != number:
+    guess = int(input("Guess a number: "))
+    if guess < number:
+            print ("Number is too low to be correct. Please try again.")
+    elif guess > number:
+            print ("Number is too high to be correct. Please try again.")
+    elif guess == number:
+            print ("Bravo! You got it right!")
+            break
+    else:
+            print ("Invalid input. Please enter a number between 1 and 10.")
