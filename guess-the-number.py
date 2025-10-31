00b96d877e2055ce249307bfc9cb23f121163c3a
Sultandammy-1 <damilolaamusan4@gmail.com>
2025-10-31 16:31:58 -0600

    Update guess-the-number.py

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,14 +1,20 @@
 import random
 guess = None
 number = random.randint(1, 10)
+print(Welcome to the Guess the Number Game!")
 while guess != number:
     guess = int(input("Guess a number: "))
     if guess < number:
-            print ("Number is too low to be correct. Please try again.")
+            print("Number is too low to be correct. Please try again.")
     elif guess > number:
-            print ("Number is too high to be correct. Please try again.")
+            print("Number is too high to be correct. Please try again.")
     elif guess == number:
-            print ("Bravo! You got it right!")
+            print("Bravo! You got it right🎉!")
             break
     else:
-            print ("Invalid input. Please enter a number between 1 and 10.")
+            print("Invalid input. Please enter a number between 1 and 10.")
+        if attempts == 3:
+            if number % 2 == 0:
+                print("Hint: The numebr is even!")
+            else:
+                print("Hint: The numebr is odd!")
